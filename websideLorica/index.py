6149f5b2dcb83051ccb71f02a5a3d96287d394c8
CRISTIAN-FG23 <CHRISTIANX23X@GMAIL.COM>
2025-07-24 11:23:33 -0500

    Add files via upload

---
+++ websideLorica/index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/acercade')
+def acercade():
+    return render_template('acercade.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
